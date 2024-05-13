d90074951b2da2e3597f879f71157d7c577a2aca
Dampel <dr.dampel@yandex.ru>
2024-05-13 21:38:15 +0300

    Api func is added.

---
+++ src/modules/photosPage/api/api.js
@@ -0,0 +1,17 @@
+import { server } from "../../../api/api";
+
+
+/**
+ * Формирует URL получения фоторафий
+ * @param {*} dateStart начальная дата выборки
+ * @param {*} dateEnd конечная дата выборки
+ * @param {*} offset кол-во отступаемых сообщений
+ * @param {*} count кол-во получаемых сообщений
+ * @param {*} search фильтр поиска
+ * @param {*} orderField колонка по которой упорядочивается
+ * @param {*} orderType тип упорядочевания
+ * @returns Сформированый URL получения фотографий
+ */
+export const GetPhotoUrl = (dateStart, dateEnd, offset, count, search, orderField, orderType) => {
+    return `${server}/api/photos?dateStart=${dateStart}&&dateEnd=${dateEnd}&&offset=${offset}&&count=${count}&&search=${search}&&orderField=${orderField}&&orderType=${orderType}`;
+}
